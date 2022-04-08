7aac1861c9a048caef7d55ed93ebdec2bdbe22b1
João Victor Pereira da Silva <vics@ynko.onmicrosoft.com>
2022-04-07 23:55:51 -0300

    Add project files.

---
+++ UniqueSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniqueSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
